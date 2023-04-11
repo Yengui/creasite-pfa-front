76602e1fcf3a7a745050d72363d075b4ccb99308
Yengui <80909453+Yengui@users.noreply.github.com>
2023-04-11 01:57:22 +0100

    template1 nav section1 section2

--- src/app/template1/template1.component.ts
+++ src/app/template1/template1.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-template1',
   templateUrl: './template1.component.html',
-  styleUrls: ['./template1.component.css']
+  styleUrls: ['./template1.component.css'],
 })
 export class Template1Component {
-
+  openList: boolean = false;
+  handleListClick() {
+    this.openList = !this.openList;
+  }
 }
