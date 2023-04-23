484683351b70ff4c8750746ff329fb1fe37fd7df
Yengui <80909453+Yengui@users.noreply.github.com>
2023-04-23 18:34:00 +0100

    template3 design responsive

--- src/app/template3/template3.component.ts
+++ src/app/template3/template3.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-template3',
   templateUrl: './template3.component.html',
-  styleUrls: ['./template3.component.css']
+  styleUrls: ['./template3.component.css'],
 })
 export class Template3Component {
-
+  openList: boolean = false;
+  handleListClick() {
+    this.openList = !this.openList;
+  }
 }
