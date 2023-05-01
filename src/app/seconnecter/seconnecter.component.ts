fd9284776acdb7d382a4cf524b9404d376067ee8
Yengui <80909453+Yengui@users.noreply.github.com>
2023-05-01 23:56:18 +0100

    login and signup fix #1

--- src/app/seconnecter/seconnecter.component.ts
+++ src/app/seconnecter/seconnecter.component.ts
@@ -1,7 +1,13 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root',
+})
 @Component({
   selector: 'app-seconnecter',
   templateUrl: './seconnecter.component.html',
@@ -11,7 +17,17 @@ export class SeconnecterComponent {
   username: string = '';
   password: string = '';
   errormsg: string = '';
-  constructor(private router: Router) {}
+  loginUser(userJson: string) {
+    const headers = new HttpHeaders().set(
+      'Content-Type',
+      'application/json; charset=utf-8'
+    );
+    return this.http.post(process.env.NG_APP_API + '/login', userJson, {
+      headers: headers,
+      observe: 'response',
+    });
+  }
+  constructor(private router: Router, private http: HttpClient) {}
   async onSubmit(form: NgForm) {
     console.log(form.value.username, form.value.email);
     if (form.value.password === '' || form.value.username === '') {
@@ -23,21 +39,35 @@ export class SeconnecterComponent {
           username: form.value.username,
           password: form.value.password,
         };
-        const res = await fetch(process.env.NG_APP_API + '/login', {
-          method: 'POST',
-          mode: 'no-cors',
-          headers: {
-            'Content-Type': 'application/json',
+        const userJson = JSON.stringify(newUser);
+        console.log(userJson);
+        this.loginUser(userJson).subscribe({
+          next: (v) => {
+            console.log(v);
+            this.errormsg = '';
+            this.router.navigate(['/create']);
           },
-          body: JSON.stringify(newUser),
+          error: (e) => {
+            console.error(e);
+            this.errormsg = "Nom d'utilisateur ou mot de passe invalide";
+          },
+          complete: () => console.info('complete'),
         });
+        // const res = await fetch(process.env.NG_APP_API + '/login', {
+        //   method: 'POST',
+        //   mode: 'no-cors',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify(newUser),
+        // });
 
-        if (res.status === 200) {
-          this.errormsg = '';
-          this.router.navigate(['/']);
-        } else {
-          this.errormsg = "Nom d'utilisateur ou mot de passe non valides";
-        }
+        // if (res.status === 200) {
+        //   this.errormsg = '';
+        //   this.router.navigate(['/create']);
+        // } else {
+        //   this.errormsg = "Nom d'utilisateur ou mot de passe non valides";
+        // }
       } catch (error) {
         console.log(error);
       }
--- src/app/sinscrire/sinscrire.component.ts
+++ src/app/sinscrire/sinscrire.component.ts
@@ -1,7 +1,13 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root',
+})
 @Component({
   selector: 'app-sinscrire',
   templateUrl: './sinscrire.component.html',
@@ -13,7 +19,17 @@ export class SinscrireComponent {
   password: string = '';
   passwordconfirmation: string = '';
   errormsg: string = '';
-  constructor(private router: Router) {}
+  registerUser(userJson: string) {
+    const headers = new HttpHeaders().set(
+      'Content-Type',
+      'application/json; charset=utf-8'
+    );
+    return this.http.post(process.env.NG_APP_API + '/register', userJson, {
+      headers: headers,
+      observe: 'response',
+    });
+  }
+  constructor(private router: Router, private http: HttpClient) {}
   async onSubmit(form: NgForm) {
     console.log(form.value.username, form.value.email);
     if (
@@ -33,21 +49,35 @@ export class SinscrireComponent {
           email: form.value.email,
           password: form.value.password,
         };
-        const res = await fetch(process.env.NG_APP_API + '/register', {
-          method: 'POST',
-          mode: 'no-cors',
-          headers: {
-            'Content-Type': 'application/json',
+        const userJson = JSON.stringify(newUser);
+        console.log(userJson);
+        this.registerUser(userJson).subscribe({
+          next: (v) => {
+            console.log(v);
+            this.errormsg = '';
+            this.router.navigate(['/login']);
           },
-          body: JSON.stringify(newUser),
+          error: (e) => {
+            console.error(e);
+            this.errormsg = "Email ou nom d'utilisateur existant";
+          },
+          complete: () => console.info('complete'),
         });
+        // const res = await fetch(process.env.NG_APP_API + '/register', {
+        //   method: 'POST',
+        //   mode: 'no-cors',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: userJson,
+        // });
 
-        if (res.status === 201) {
-          this.errormsg = '';
-          this.router.navigate(['/login']);
-        } else {
-          this.errormsg = "Email ou nom d'utilisateur existant";
-        }
+        // if (res.status === 201) {
+        //   this.errormsg = '';
+        //   this.router.navigate(['/login']);
+        // } else {
+        //   this.errormsg = "Email ou nom d'utilisateur existant";
+        // }
       } catch (error) {
         console.log(error);
       }
